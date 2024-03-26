aa637256a5b6b4da77f7dd2a1cd09097ae82d3ec
emilianogastaldo <emiliano0303@gmail.com>
2024-03-26 15:16:34 +0100

    creato il seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,22 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Creo l'array delle tecnologie su cui creero le istanze(tuple) per riempire la tabella
+        $techs = [
+            ['label' => 'HTML', 'color' => 'danger'],
+            ['label' => 'ES6', 'color' => 'secondary'],
+            ['label' => 'CSS', 'color' => 'success'],
+            ['label' => 'Bootstrap', 'color' => 'primary'],
+            ['label' => 'Vue', 'color' => 'warning'],
+            ['label' => 'PHP', 'color' => 'info'],
+            ['label' => 'SQL', 'color' => 'black'],
+            ['label' => 'Laravel', 'color' => 'dark']
+        ];
+        foreach ($techs as $tech) {
+            $new_tech = new Technology();
+            $new_tech->label = $tech['label'];
+            $new_tech->color = $tech['color'];
+            $new_tech->save();
+        }
     }
 }
